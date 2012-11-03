da4937b116a07e44f801501637668c436e453d07
unknown <Don@Don-PC.(none)>
2012-11-03 07:42:09 -0600

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AddressBook::Application.config.secret_token = 'f01065fe5ebe8a78fd0328023fb7af066c41b5994c36446294c41416772414ad7acd82501b19f479382d9e67368317e0b3a97132795a2b75ed22421ab7ecec41'
